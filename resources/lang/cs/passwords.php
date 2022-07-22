2670d99e0902fb586456577d01780a7394bcb748
Eliska Ryklova <ryklovae@coventry.ac.uk>
2022-07-22 17:58:18 +0100

    Překlad chybových hlášek

---
+++ resources/lang/cs/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Vaše heslo bylo obnoveno!',
+    'sent' => 'Na email jsme Vám poslali odkaz pro změnu hesla!',
+    'throttled' => 'Prosím počkejte.',
+    'token' => 'Teto token pro změnu hesla je neplatný.',
+    'user' => "Nemůžeme najít uživatele s touto emailovou adresou.",
+
+];
