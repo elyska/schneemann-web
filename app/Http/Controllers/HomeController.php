5066e05bb7fa71fa41ff7bf4db1904d8462892f7
Eliska Ryklova <ryklovae@coventry.ac.uk>
2022-07-26 13:18:54 +0100

    Trust the device after registration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 
 class HomeController extends Controller
 {
@@ -21,8 +23,25 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
+        $email = Auth::user()->email;
+        if($request->hasCookie('trusted-device')) {
+            // get trusted-device cookie
+            $cookie = $request->cookie('trusted-device');
+            // cookie value to array
+            $emails = json_decode($cookie);
+            // add new email address
+            array_push($emails, $email);
+            // cookie value to json string
+            $cookieValue = json_encode($emails);
+        }
+        // if the trusted-device cookie is not set, create an array with the new email address
+        else {
+            $cookieValue = json_encode(array($email));
+        }
+        Cookie::queue('trusted-device', $cookieValue, 43800);
+
         return view('home');
     }
 }
