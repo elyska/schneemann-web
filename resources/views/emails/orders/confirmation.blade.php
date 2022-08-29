7c08012df2c0992510229856b7ff66e1ac94542b
Eliska Ryklova <ryklovae@coventry.ac.uk>
2022-08-29 19:33:25 +0100

    Order confirmation email

--- resources/views/emails/orders/confirmation.blade.php
+++ resources/views/emails/orders/confirmation.blade.php
@@ -1,54 +1,59 @@
-@component('mail::message')
-Dear Customer,
 
-thank you for your order. You can find the order details below.
+<p>
+    Dear Customer,
 
-@component('mail::table')
-    | Your Order    |               |          |
-    | :------------ |---------------| ---------|
-    @foreach($products as $product)
-    | {{ $product->title_cz }} | {{ $product->quantity }} pcs | {{ $product->price * $product->quantity }} EUR |
-    @endforeach
-    | Subtotal      |       | {{ $subtotalEUR }} EUR |
-    |  Shipping     |       | {{ $postageEUR }} EUR |
-    |    Total      |       | {{ $subtotalEUR + $postageEUR }} EUR |
-@endcomponent
+    <br />
+    <br />
+    thank you for your order. You can find the order details below.
+</p>
 
-
-{!! '<table style="width: 100%">' !!}
+<h1>Your Order</h1>
+<table style="width: 100%">
 @foreach($products as $product)
-    {!! "<tr>" !!}
-
-    {!! "<td>" !!}
-    {{ $product->title_cz }}
-    {!! "</td>" !!}
-    {!! "<td>" !!}
-    {{ $product->quantity }} ks
-    {!! "</td>" !!}
-    {!! "<td>" !!}
-    {{ $product->price * $product->quantity }} EUR
-    {!! "</td>" !!}
-
-    {!! "</tr>" !!}
+    <tr>
+        <td style="border-top: 1px solid #edeff2">
+            {{ $product->title_cz }} @if($product->colour) <br /> @endif
+            {{ $product->colour }} @if($product->size) <br /> @endif
+            {{ $product->size }} <br />
+        </td>
+        <td style="border-top: 1px solid #edeff2">
+            {{ $product->quantity }} ks
+        </td>
+        <td style="border-top: 1px solid #edeff2">
+            {{ $product->price * $product->quantity }} EUR
+        </td>
+    </tr>
 @endforeach
-{!! "</table>" !!}
-
-{!! '<table style="width: 100%">' !!}
-@foreach($products as $product)
-    {!! '<tr>' !!}
-        {!! '<td style="border-top: 1px solid #edeff2">' !!}
+    <tr>
+        <td colspan="2" style="border-top: 1px solid #edeff2">
+            Subtotal
+        </td>
+        <td style="border-top: 1px solid #edeff2">
+            {{ $subtotalEUR }} EUR
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2">
+            Shipping
+        </td>
+        <td>
+            {{ $postageEUR }} EUR
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2">
             Total
-        {!! "</td>" !!}
-        {!! '<td style="border-top: 1px solid #edeff2">' !!}
+        </td>
+        <td>
             {{ $subtotalEUR + $postageEUR }} EUR
-        {!! "</td>" !!}
-    {!! "</tr>" !!}
-@endforeach
-{!! "</table>" !!}
+        </td>
+    </tr>
+</table>
 
 <br />
-<br />
 
-Kind regards, <br />
-{{ config('app.name') }}
-@endcomponent
+<p>
+    Kind regards, <br />
+    {{ config('app.name') }}
+</p>
+
